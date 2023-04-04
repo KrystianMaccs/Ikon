ea6f11b1f666496d1206473dfd6aac8ae48f77c5
Krystian Maccs <krystianmaccs@gmail.com>
2023-04-04 03:52:29 +0100

    feat: Refactored user app to include more features

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,40 +1,92 @@
 from django.contrib.auth import get_user_model
-from django_countries.serializer_fields import CountryField
-from djoser.serializers import UserCreateSerializer
-from phonenumber_field.serializerfields import PhoneNumberField
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
-    first_name = serializers.SerializerMethodField()
-    last_name = serializers.SerializerMethodField()
+
+class ConfirmEmailSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(min_length=1, write_only=True)
+    uidb64 = serializers.CharField(min_length=1, write_only=True)
 
     class Meta:
         model = User
-        fields = [
-            "id",
-            "username",
-            "email",
-            "first_name",
-            "last_name",
-        ]
+        fields = ['token', 'uidb64']
+        
 
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer): 
+
+    """Override default token login to include user data"""
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        user = self.user
+        if not user.is_verified:
+            raise serializers.ValidationError({"error":"Email is not verified."})
+
+        data.update(
+            {
+                "id": self.user.id,
+                "email": self.user.email,
+                "first_name": self.user.first_name,
+                "last_name": self.user.last_name,
+                "is_superuser": self.user.is_superuser,
+                "is_staff": self.user.is_staff,
+            }
+        )
+
+        return data
+    
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, style={'input_type':'password'})
+
+    # Meta class to specify the model and its fields to be serialized
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'password'] 
+
+    # Method to validate the email entered by the user
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with this email address already exists.")
+        return value
+    
+    # Method to validate the password entered by the user
+    def validate_password(self, value):
+        try:
+            validate_password(value)
+        except ValidationError as e:
+            raise serializers.ValidationError(e.messages)
+        return value
+    
+    # Create a new user object using the validated data
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email = validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password'],
+        )
+        return user
+    
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'is_active']
+        
     def get_first_name(self, obj):
         return obj.first_name.title()
 
     def get_last_name(self, obj):
         return obj.last_name.title()
-
-    def to_representation(self, instance):
-        representation = super(UserSerializer, self).to_representation(instance)
-        if instance.is_superuser:
-            representation["admin"] = True
-        return representation
-
-
-class CreateUserSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
-        model = User
-        fields = ["id", "username", "email", "first_name", "last_name", "password"]
