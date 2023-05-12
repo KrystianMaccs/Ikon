4a19e1924c178a3671354dabe29395ef09fc8b83
Krystian Maccs <krystianmaccs@gmail.com>
2023-05-12 22:56:48 +0100

    chore: docker, makefiles, test files

--- apps/profiles/signals.py
+++ apps/profiles/signals.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from apps.profiles.models import Profile
-from real_estate.settings.base import AUTH_USER_MODEL
+from ikon.settings.base import AUTH_USER_MODEL
 
 logger = logging.getLogger(__name__)
 
